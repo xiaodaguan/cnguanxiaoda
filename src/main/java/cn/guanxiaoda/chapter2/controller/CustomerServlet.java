40319eeba2c00d47179be39a5b5412fdca3a147c
guanxiaoda <xiaodaguan@gmail.com>
2017-05-22 21:54:42 +0800

    try deploy

--- src/main/java/cn/guanxiaoda/chapter2/controller/CustomerServlet.java
+++ src/main/java/cn/guanxiaoda/chapter2/controller/CustomerServlet.java
@@ -1,11 +1,15 @@
 package cn.guanxiaoda.chapter2.controller;
 
+import cn.guanxiaoda.chapter2.model.Customer;
+import cn.guanxiaoda.chapter2.service.CustomerService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by guanxiaoda on 17/5/19.
@@ -13,13 +17,26 @@ import java.io.IOException;
 @WebServlet("/customer")
 public class CustomerServlet extends HttpServlet {
 
+    private CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException {
+        //todo 用单例模式优化
+        customerService = new CustomerService();
+    }
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 // todo
+        List<Customer> customerList = customerService.getCustomerList("");
+        req.setAttribute("customerList", customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req, resp);
+
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //todo
     }
 }
